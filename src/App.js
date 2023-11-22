34c52d7b08ff01d472a2256e3853a543b8680fce
Kevin <kevinponting82@gmail.com>
2023-11-22 00:09:21 -0800

    basically done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './assets/App.css'
 import Panel from './components/Panel';
+import TicTacToe from './components/TicTacToe';
 
 function App() {
   return (
     <div className="App">
       <Panel />
+      <TicTacToe />
     </div>
   );
 }
